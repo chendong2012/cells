e04f89bbd5819c1928dff6646c763adc0749ae40
chen dong <china.gd.sz.cd@gmail.com>
2012-11-08 23:43:39 -0500

    加入测试用例

--- ds2460/usbcontrol/main.cpp
+++ ds2460/usbcontrol/main.cpp
@@ -6,8 +6,12 @@
 #include <syslog.h>
 #include "Secrect.h"
 
+#include <getopt.h>
+#include <unistd.h>
+
 #define EEPROM_PAGE 14
 #define EEPROM_PAGE_SIZE 8
+
 struct secrect_device_context {
 	unsigned char romid[8];
 	unsigned char mac_code[20];
@@ -23,7 +27,9 @@ struct secrect_device_context {
 Secrect mysecrect;
 struct secrect_device_context ctx;
 /*=========================================*/
-
+/*
+ * 读设备的ROMID
+ * */
 int test_read_romid(void)
 {
 	int i = 0;
@@ -40,6 +46,9 @@ int test_read_romid(void)
 	return rc; 
 }
 
+/*
+ *读设备计算出来的mac认证码
+ * */
 int test_read_mac_code(void)
 {
 	int i = 0;
@@ -55,24 +64,32 @@ int test_read_mac_code(void)
 	return rc; 
 }
 
+/*
+ *读eeprom数据
+ *
+ * */
 int test_read_eeprom(void)
 {
 	int i = 0;
 	int rc;
 	unsigned char eeprom[128];
 	memset(eeprom, 0x00, 128);
-	rc = mysecrect.read_eeprom(0x80, eeprom, 8);
+	rc = mysecrect.read_eeprom(0x80, eeprom, 16);
 	if (rc < 0) {
 		fprintf(stderr, "%d:%s error\n",__LINE__, __func__);
 		return -1;
 	}
-	for (i = 0; i < 8; i++) {
+	for (i = 0; i < 16; i++) {
 		fprintf(stdout, "%d:0x%02x ", i, eeprom[i]);
 	}
 	printf("\n");
 	return rc; 
 }
 
+/*
+ *写eeprom数据
+ *
+ * */
 int test_write_eeprom(void)
 {
 	int i = 0;
@@ -94,6 +111,9 @@ int test_write_eeprom(void)
 	return rc; 
 }
 
+/*
+ *写用于计算mac码的原始数据
+ * */
 int test_write_input_data(void)
 {
 	int i = 0;
@@ -116,6 +136,10 @@ int test_write_input_data(void)
 	return rc; 
 }
 
+/*
+ *读用于计算mac认证码的原始数据
+ *
+ * */
 int test_read_input_data(void)
 {
 	int i = 0;
@@ -138,17 +162,46 @@ int test_read_input_data(void)
 	return rc; 
 }
 
+
+struct option options[] = {
+	{"type", required_argument, NULL, 't'},
+	{"read", required_argument, NULL, 'r'},
+	{0, 0, 0, 0},
+};
+
 int main(int argc, char *argv[])
 {
+
+	int c;
+	char *l_opt_arg;
 	memset(&ctx, 0x00, sizeof(ctx));
-	test_read_romid();
-	test_read_mac_code();
+	while((c = getopt_long(argc, argv, ":r:t:", options, NULL)) != -1){
+		switch (c){
+			case 't':
+				l_opt_arg = optarg;
+				printf("arg is %s\n", optarg);
+				test_read_eeprom();
+				printf("\n");
+				break;
+			case 'r':
+				l_opt_arg = optarg;
+				printf("arg is %s\n", optarg);
+				test_read_eeprom();
+				printf("\n");
+				break;
+			default:
+				printf("input error\n");
+				break;
+		}
+	}
+
+
+//	test_read_romid();
+//	test_read_mac_code();
 	//
 //	test_write_eeprom();
-//	test_read_eeprom();
-	test_write_input_data();
-	test_read_input_data();
-	printf("\n");
+//	test_write_input_data();
+//	test_read_input_data();
 
 	return 1;
 }
