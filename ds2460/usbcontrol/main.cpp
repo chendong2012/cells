17e88e96c9497c0d83556a8c9efe0d24837eee8b
chen dong <china.gd.sz.cd@gmail.com>
2012-11-11 23:39:00 -0500

    modify main.cpp

--- ds2460/usbcontrol/main.cpp
+++ ds2460/usbcontrol/main.cpp
@@ -28,7 +28,9 @@ Secrect mysecrect;
 struct secrect_device_context ctx;
 /*=========================================*/
 /*
- * 读设备的ROMID
+ *read device romid
+ *return -1 fail
+ *return 1 success
  * */
 int test_read_romid()
 {
@@ -36,7 +38,7 @@ int test_read_romid()
 	int rc;
 	rc = mysecrect.read_rom_id(ctx.romid, sizeof(ctx.romid));
 	if (rc < 0) {
-		fprintf(stderr, "%d:%s error\n",__LINE__, __func__);
+		fprintf(stderr, "%d:%s error\n", __LINE__, __func__);
 	}
 	for (i = 0; i < 8; i++) {
 		printf("%02x ", ctx.romid[i]);
@@ -45,6 +47,11 @@ int test_read_romid()
 	return rc; 
 }
 
+/*
+ *read device mac code [20 bytes]
+ *return -1 fail
+ *return 1 success
+ * */
 int test_read_mac_code(void)
 {
 	int i = 0;
@@ -52,7 +59,7 @@ int test_read_mac_code(void)
 	printf("\n======>secrect read mac code test:\n");
 	rc = mysecrect.read_mac_code(ctx.mac_code, sizeof(ctx.mac_code));
 	if (rc < 0) {
-		fprintf(stderr, "%d:%s error\n",__LINE__, __func__);
+		fprintf(stderr, "%d:%s error\n", __LINE__, __func__);
 	}
 	for (i = 0; i < 20; i++) {
 		if (i%8 == 0)
@@ -64,8 +71,10 @@ int test_read_mac_code(void)
 }
 
 /*
- *读eeprom数据
- *
+ *read eeprom data
+ *page range: [0,1,2,3...10]
+ *return -1 fail
+ *return 1 success
  * */
 int test_read_eeprom(int page, int len)
 {
@@ -75,7 +84,7 @@ int test_read_eeprom(int page, int len)
 	memset(eeprom, 0x00, 128);
 	rc = mysecrect.read_eeprom(0x80 + 8*page, eeprom, len);
 	if (rc < 0) {
-		fprintf(stderr, "%d:%s error\n",__LINE__, __func__);
+		fprintf(stderr, "%d:%s error\n", __LINE__, __func__);
 		return -1;
 	}
 	for (i = 0; i < len; i++) {
@@ -86,10 +95,11 @@ int test_read_eeprom(int page, int len)
 }
 
 /*
- *
- *写eeprom数据
- * 0 <= page <=10  8 bytes per page
- * 
+ *write eeprom data
+ *one page size is 8 bytes
+ *page range: [0,1,2,3...10]
+ *return -1 fail
+ *return 1 success
  * */
 int test_write_eeprom(int page, unsigned char *data, int data_len)
 {
@@ -97,7 +107,7 @@ int test_write_eeprom(int page, unsigned char *data, int data_len)
 	int rc;
 	rc = mysecrect.write_eeprom(0x80 + page*8, data, data_len);
 	if (rc < 0) {
-		fprintf(stderr, "%d:%s error\n",__LINE__, __func__);
+		fprintf(stderr, "%d:%s error\n", __LINE__, __func__);
 		return -1;
 	}
 
@@ -111,7 +121,101 @@ int test_write_eeprom(int page, unsigned char *data, int data_len)
 }
 
 /*
- *写用于计算mac码的原始数据
+ *write s secrect len is 8 bytes
+ *return -1 fail
+ *return 1 success
+ * */
+int test_write_s_secrect(unsigned char *secrect, int secrect_len)
+{
+	int i;
+	int rc;
+	rc = mysecrect.write_s_secrect(secrect, secrect_len);
+	if (rc < 0) {
+		fprintf(stderr, "%d:%s error\n", __LINE__, __func__);
+		return -1;
+	}
+	for (i = 0; i < secrect_len; i++) {
+		if (i%8 == 0 && i != 0)
+			printf("\n");
+		fprintf(stdout, "%02x ", secrect[i]);
+	}
+	printf("\n");
+	return rc;
+}
+
+/*
+ *write e esecrect1 len is 8 bytes
+ *return -1 fail
+ *return 1 success
+ * */
+int test_write_e_secrect1(unsigned char *secrect, int secrect_len)
+{
+	int i;
+	int rc;
+	rc = mysecrect.write_e_secrect1(secrect, secrect_len);
+	if (rc < 0) {
+		fprintf(stderr, "%d:%s error\n",__LINE__, __func__);
+		return -1;
+	}
+	for (i = 0; i < secrect_len; i++) {
+		if (i%8 == 0 && i != 0)
+			printf("\n");
+		fprintf(stdout, "%02x ", secrect[i]);
+	}
+	printf("\n");
+	return rc;
+}
+
+/*
+ *write e esecrect2 len is 8 bytes
+ *return -1 fail
+ *return 1 success
+ * */
+int test_write_e_secrect2(unsigned char *secrect, int secrect_len)
+{
+	int i;
+	int rc;
+	rc = mysecrect.write_e_secrect2(secrect, secrect_len);
+	if (rc < 0) {
+		fprintf(stderr, "%d:%s error\n",__LINE__, __func__);
+		return -1;
+	}
+	for (i = 0; i < secrect_len; i++) {
+		if (i%8 == 0 && i != 0)
+			printf("\n");
+		fprintf(stdout, "%02x ", secrect[i]);
+	}
+	printf("\n");
+	return rc;
+}
+
+/*
+ *write e esecrect3 len is 8 bytes
+ *return -1 fail
+ *return 1 success
+ * */
+int test_write_e_secrect3(unsigned char *secrect, int secrect_len)
+{
+	int i;
+	int rc;
+	rc = mysecrect.write_e_secrect3(secrect, secrect_len);
+	if (rc < 0) {
+		fprintf(stderr, "%d:%s error\n",__LINE__, __func__);
+		return -1;
+	}
+	for (i = 0; i < secrect_len; i++) {
+		if (i%8 == 0 && i != 0)
+			printf("\n");
+		fprintf(stdout, "%02x ", secrect[i]);
+	}
+	printf("\n");
+	return rc;
+}
+
+/*
+ *write input raw datas for creating a mac code
+ *return -1 fail
+ *return 1 success
  * */
 int test_write_input_data(unsigned char *data, int len)
 {
@@ -119,7 +223,7 @@ int test_write_input_data(unsigned char *data, int len)
 	int rc;
 	rc = mysecrect.write_input_data(data,len);
 	if (rc < 0) {
-		fprintf(stderr, "%d:%s error\n",__LINE__, __func__);
+		fprintf(stderr, "%d:%s error\n", __LINE__, __func__);
 		return -1;
 	}
 
@@ -133,6 +237,11 @@ int test_write_input_data(unsigned char *data, int len)
 	return rc; 
 }
 
+/*
+ *read input raw data
+ *return -1 fail
+ *return 1 success
+ * */
 int test_read_input_data(void)
 {
 	int i = 0;
@@ -141,7 +250,7 @@ int test_read_input_data(void)
 	memset(input, 0x00, 47);
 	rc = mysecrect.read_input_data(input, 47);
 	if (rc < 0) {
-		fprintf(stderr, "%d:%s error\n",__LINE__, __func__);
+		fprintf(stderr, "%d:%s error\n", __LINE__, __func__);
 		return -1;
 	}
 
@@ -161,17 +270,9 @@ struct option options[] = {
 	{"write", required_argument, NULL, 'w'},
 	{"page", required_argument, NULL, 'p'},
 	{"length", required_argument, NULL, 'l'},
+	{"help", no_argument, NULL, 'h'},
 	{0, 0, 0, 0},
 };
-
-int analyse_read_eeprom(int begin, int end, char **av)
-{
-	int i;
-	for (i = begin; i < end; i++) {
-		printf("args is %d:%s\n", i, av[i]);
-	}
-	return 0;
-}
 
 struct cmdline {
 	unsigned char data[255];
@@ -193,14 +294,45 @@ int main(int argc, char *argv[])
 	unsigned char cmd_data[255];
 	int cmd_len = 0xff;
 	
+	if (argc == 1) {
+		printf("usage: \n"
+			"	uway [-w types] [-r types] [-l size] [-p page] [datas]\n");
+		printf("types:\n"
+			"	eeprom\n"
+			"	mac\n"
+			"	ssecrect\n"
+			"	esecrect1\n"
+			"	esecrect2\n"
+			"	esecrect3\n");
+		printf("size:\n"
+			"	write how many datas into device\n");
+		printf("page:\n"
+			"	eeprom page number\n");
+		printf("datas:\n"
+			"	datas for writing into device\n");
+		return 0;
+	}
 	memset(&ctx, 0x00, sizeof(ctx));
-	while((c = getopt_long(argc, argv, ":r:w:p:l:", options, NULL)) != -1){
+	while((c = getopt_long(argc, argv, ":r:w:p:l:-h", options, NULL)) != -1){
 		switch (c){
-		case 'w':
+		case 'w':	/*list write types*/
 			if (strcmp(optarg, "eeprom") == 0) {
 				w_type = 1;
+
 			} else if (strcmp(optarg, "mac") == 0){
 				w_type = 2;
+
+			} else if (strcmp(optarg, "ssecrect") == 0){
+				w_type = 3;
+
+			} else if (strcmp(optarg, "esecrect1") == 0){
+				w_type = 4;
+
+			} else if (strcmp(optarg, "esecrect2") == 0){
+				w_type = 5;
+
+			} else if (strcmp(optarg, "esecrect3") == 0){
+				w_type = 6;
 
 			} else {
 				printf("error type\n");
@@ -208,7 +340,7 @@ int main(int argc, char *argv[])
 			w_pos = optind;
 			break;
 
-		case 'r':
+		case 'r':	/*list read types*/
 			if (strcmp(optarg, "romid") == 0) {
 				r_type = 1;
 			} else if (strcmp(optarg, "mac") == 0) {
@@ -221,29 +353,47 @@ int main(int argc, char *argv[])
 			r_pos = optind;
 			break;
 
-		case 'p':
+		case 'p':	/*page for eeprom data area*/
 			cmd_page = strtol(optarg, NULL, 0);
 			break;
 
-		case 'l':
+		case 'l':	/*write into device data lenght*/
 			cmd_len = strtol(optarg, NULL, 0);
 			break;
+		case 'h':
+			printf("usage: \n"
+			"	uway [-w types] [-r types] [-l size] [-p page] [datas]\n");
+			printf("types:\n"
+				"	eeprom\n"
+				"	mac\n"
+				"	ssecrect\n"
+				"	esecrect1\n"
+				"	esecrect2\n"
+				"	esecrect3\n");
+			printf("size:\n"
+				"	write how many datas into device\n");
+			printf("page:\n"
+				"	eeprom page number\n");
+			printf("datas:\n"
+				"	datas for writing into device\n");
+			return 0;
 
-		default:
+		default:	/*illeage parameters*/
 			fprintf(stderr, "input error\n");
-			break;
+			return 1;
 		}
 	}
 
+	/*run the commands on the command line in order*/
 	for (argc_count = 0; argc_count < argc; argc_count++) {
 		if (w_pos < r_pos) {
-			switch (w_type) {
-			case 1:
+
 				if (cmd_len  != (argc - optind)) {
 					fprintf(stderr, "please input lenght not in range\n");
 					return 1;
 				}
-
+			switch (w_type) {
+			case 1:
 				for (i = optind, j=0; i < argc; i++, j++) {
 					cmd_data[j] = strtol(argv[i], NULL, 0); 
 				}
@@ -254,23 +404,40 @@ int main(int argc, char *argv[])
 				}
 				test_write_eeprom(cmd_page, cmd_data, cmd_len);
 				break;
-			case 2:
-				if (cmd_len  != (argc - optind)) {
-					fprintf(stderr, "please input lenght not in range\n");
-					return 1;
-				}
 
+			case 2:
 				for (i = optind, j=0; i < argc; i++, j++) {
 					cmd_data[j] = strtol(argv[i], NULL, 0); 
 				}
 				test_write_input_data(cmd_data, cmd_len);
-
 				break;
 			case 3:
+				for (i = optind, j=0; i < argc; i++, j++) {
+					cmd_data[j] = strtol(argv[i], NULL, 0); 
+				}
+				test_write_s_secrect(cmd_data, cmd_len);
+				break;
+			case 4:
+				for (i = optind, j=0; i < argc; i++, j++) {
+					cmd_data[j] = strtol(argv[i], NULL, 0); 
+				}
+				test_write_e_secrect1(cmd_data, cmd_len);
+				break;
+			case 5:
+				for (i = optind, j=0; i < argc; i++, j++) {
+					cmd_data[j] = strtol(argv[i], NULL, 0); 
+				}
+				test_write_e_secrect2(cmd_data, cmd_len);
+				break;
+			case 6:
+				for (i = optind, j=0; i < argc; i++, j++) {
+					cmd_data[j] = strtol(argv[i], NULL, 0); 
+				}
+				test_write_e_secrect3(cmd_data, cmd_len);
 				break;
 			default:
 				break;
-			} 
+			}
 			w_pos = 0xff;
 		} else if (w_pos > r_pos) {
 			switch (r_type) {
