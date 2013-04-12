8a693e4b5c0ef73f2aa6be573690f09417cd1cde
chen dong <china.gd.sz.cd@gmail.com>
2013-04-12 22:46:07 +0800

    加入review的代码编译并发布出去

---
+++ checkout/review.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#into mtkprojects directory
+cd "$1"
+prj_info="prj_info"
+allreview=(`find -maxdepth 1 -type d | sed -n '/review$/p' | sed 's/\.\///'`)
+for prjdirname in "$allreview[@]"; do
+	cd "$prjdirname"
+	prjpath=`getitem.sh "$prj_info" "prjpath"`
+	cd "$mypath"
+	project=`getitem.sh "$prj_info" "project"`
+	cd $p
+	./mk  $prj n
+	reviewoutput=`getitem.sh "$prj_info" "reviewoutput"`
+
+	if [[ "$reviewoutput" != "" ]]; then
+		reviewoutput.sh "$PWD" "$prjdirname" "$reviewoutput"
+	fi
+done
